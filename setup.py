dbd495890e9d2c341682f7046073eb0d93b60cd8
Chris Glass <christopher.glass@divio.ch>
2011-04-01 12:55:00 +0200

    Made README.rst an rst file, add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+CLASSIFIERS = []
+
+setup(
+    author="Christopher Glass",
+    author_email="tribaal@gmail.com",
+    name='django-shop-paypal',
+    version='0.0.0',
+    description='An paypal payment backend wrapping django-paypal',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='http://www.django-shop.org/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.2',
+    ],
+    packages=find_packages(exclude=["example", "example.*"]),
+    zip_safe = False
+)
