e84df5284cf5e146b0ef10e04ec82756d510de7c
Chris Glass <christopher.glass@divio.ch>
2011-04-01 14:19:09 +0200

    Removed useless files

--- shop_paypal/urls.py
+++
@@ -1,7 +0,0 @@
-#-*- coding: utf-8 -*-
-''' A normal URLs file, like you would do for any Django application. '''
-from django.conf.urls.defaults import patterns, url
-
-ulrpatterns = patterns('',
-    url(r'^$', my_view),
-)
